5127f2668e513e5704c453b6154b8aa081843b3b
Shailesh Kolap <skolap22@gmail.com>
2023-01-11 17:27:16 +0530

    Added component in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import CreateCustomerAccount from "./Components/CreateCustomerAccount";
 function App() {
-  return <div className="App">Start Here</div>;
+  return (
+    <div className="App">
+      Start Here
+      <CreateCustomerAccount />
+    </div>
+  );
 }
 
 export default App;

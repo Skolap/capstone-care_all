028a4c8f1a0080c872813c8cf39608da54fa31e1
unknown <J4sumit@gmail.com>
2023-01-21 14:49:21 +0530

    Search component created

---
+++ src/Components/SearchForCareTaker/SearchResulls.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import sitterInformation from "../../EmployeeInformation";
+import SearchResultCard from "./SearchResultCard";
+
+const SearchResults = () => {
+  //Get data from MoreInfo.js
+  const location = useLocation();
+  //save data in allinfo
+  const [allInfo, setAllInfo] = useState(location.state);
+  //Filter the data for conditions
+  const x = sitterInformation.filter(
+    (sitter) =>
+      Number(allInfo.pin) >= sitter.OperatingPin1 &&
+      Number(allInfo.pin) <= sitter.OperatingPin2 &&
+      sitter.Experience > Number(allInfo.MinExperience)
+  );
+
+  console.log(`XLength: ${x.length}`);
+
+  const CreateCard = (x2) => {
+    return (
+      <SearchResultCard
+        key={x2.ID}
+        Name={x2.Name}
+        Profile={x2.Profile}
+        Address={x2.Address}
+        Gender={x2.Gender}
+        Experience={x2.Experience}
+        Price={x2.Price}
+        Phone={x2.Phone}
+      />
+    );
+  };
+
+  return (
+    <>
+      {x.length > 0 ? x.map(CreateCard) : <div>No Data Found</div>}
+      {/* {x.map(CreateCard)} */}
+    </>
+  );
+};
+
+export default SearchResults;

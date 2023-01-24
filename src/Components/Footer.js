7469f4cd54fdcd944fed50c477c275b511424750
Shailesh Kolap <skolap22@gmail.com>
2023-01-24 18:01:12 +0530

    Complated footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="bg-gray-800 text-gray-300 mt-4">
+    <footer className="">
+      <div className="bg-gray-800 text-gray-300 mt-4 w-full absolute">
         <p className="p-2 text-center">
           {new Date().getFullYear()} &copy; Copyright: CareAll.com
         </p>

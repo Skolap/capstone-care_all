6ffc6a4a5259f1109f912201ef3b31db007918b2
Shailesh Kolap <skolap22@gmail.com>
2023-01-24 18:01:44 +0530

    Complated JoinNow

--- src/Components/JoinNow.js
+++ src/Components/JoinNow.js
@@ -6,6 +6,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../FirebaseConfigs/firebaseConfig";
 import { collection, addDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import Footer from "./Footer";
 
 const JoinNow = () => {
   //Define Hooks
@@ -57,7 +58,7 @@ const JoinNow = () => {
             setErrorMsg("");
             setTimeout(() => {
               setSuccessMsg("");
-              navigate("/home");
+              navigate("/login");
             }, 4000);
           })
           .catch((error) => {
@@ -77,54 +78,80 @@ const JoinNow = () => {
   return (
     <div>
       <Navbar />
-      <div>
-        <form action="" onSubmit={submitHandler}>
-          <p>Create Account</p>
+      <div className="w-max mx-auto">
+        <p className="text-2xl text-center">Create Account</p>
+        <div className="flex flex-col items-center p-20 bg-red-700 max-w-max mx-auto text-right">
+          <form action="" onSubmit={submitHandler}>
+            {successMsg && (
+              <>
+                <div className="bg-green-600 bg-opacity-50 p-1 text-center text-white">
+                  {successMsg}
+                </div>
+              </>
+            )}
+            {errorMsg && (
+              <>
+                <div className="bg-orange-400 bg-opacity-50 p-1 text-center text-white">
+                  {errorMsg}
+                </div>
+              </>
+            )}
+            <div className="flex flex-col items-center">
+              <div className="p-3">
+                <div className="text-right p-1">
+                  <label className="p-4">Your Name</label>
+                  <input
+                    className="p-1 outline outline-1"
+                    onChange={nameChangeHandler}
+                    type="text"
+                    placeholder="Enter Your Name"
+                  />
+                </div>
+                <div className="text-right p-1">
+                  <label className="p-4">Mobile Number</label>
+                  <input
+                    className="p-1 outline outline-1"
+                    type="tel"
+                    onChange={mobileChangeHandler}
+                    placeholder="Mobile Number"
+                  />
+                </div>
 
-          {successMsg && (
-            <>
-              <div className="success-msg">{successMsg}</div>
-            </>
-          )}
-          {errorMsg && (
-            <>
-              <div className="error-msg">{errorMsg}</div>
-            </>
-          )}
+                <div className="text-right p-1">
+                  <label className="p-4">Email</label>
+                  <input
+                    className="p-1 outline outline-1"
+                    type="email"
+                    onChange={emailChangeHandler}
+                    placeholder="Enter Your Email"
+                  />
+                </div>
 
-          <label htmlFor="">Your Name </label>
-          <input
-            onChange={nameChangeHandler}
-            type="text"
-            placeholder="Enter Your Name"
-          />
-          <label htmlFor="">Mobile Number</label>
-          <input
-            type="tel"
-            onChange={mobileChangeHandler}
-            placeholder="Mobile Number"
-          />
-
-          <label htmlFor="">Email</label>
-          <input
-            type="email"
-            onChange={emailChangeHandler}
-            placeholder="Enter Your Email"
-          />
-
-          <label htmlFor="">Password</label>
-          <input
-            type="tel"
-            onChange={passwordChangeHandler}
-            placeholder="Choose Your Password"
-          />
-          <button type="submit">Submit</button>
-          <div>
-            <span>Alredy Have Account?</span>
-            <Link to="/login">Login</Link>
-          </div>
-        </form>
+                <div className="text-right p-1">
+                  <label className="p-4">Password</label>
+                  <input
+                    className="p-1 outline outline-1"
+                    type="tel"
+                    onChange={passwordChangeHandler}
+                    placeholder="Choose Your Password"
+                  />
+                </div>
+              </div>
+              <button
+                className="py-2 px-5 bg-slate-50 rounded-lg"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+            <div className="text-white">
+              <span>Alredy Have Account? </span>
+              <Link to="/login">Login</Link>
+            </div>
+          </form>
+        </div>
       </div>
+      {/* <Footer /> */}
     </div>
   );
 };

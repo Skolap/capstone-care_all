5060760bb134dff10d478543d2d707775eb4a32e
Shailesh Kolap <skolap22@gmail.com>
2023-01-24 18:03:57 +0530

    Created EmployeeInformation.js

---
+++ src/Components/EmployeeInformation.js
@@ -0,0 +1,310 @@
+const sitterInformation = [
+  {
+    ID: 1,
+    Name: "Sushmita Verma",
+    Address: "26, Nitin Heights, Alex Chowk Kochi",
+    Phone: "009 12641707",
+    Gender: "Female",
+    OperatingPin1: 400000,
+    OperatingPin2: 450000,
+    Age: 30,
+    Rating: 4,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://www.shutterstock.com/image-photo/headshot-portrait-smiling-young-indian-260nw-2029044050.jpg",
+  },
+  {
+    ID: 2,
+    Name: "Ambika Bawa",
+    Address: "48, Aundh, Jamnagar",
+    Phone: "+91 3058469509",
+    Gender: "Female",
+    OperatingPin1: 450000,
+    OperatingPin2: 500000,
+    Age: 35,
+    Rating: 3,
+    Experience: 1,
+    Price: 200,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://www.shutterstock.com/image-photo/profile-picture-smiling-millennial-indian-260nw-1826745269.jpg",
+  },
+  {
+    ID: 3,
+    Name: "Dhiraj Kumar Pathak",
+    Address: "24, Mehul Chowk, Gangtok",
+    Phone: "+91 3058469509",
+    Gender: "Male",
+    OperatingPin1: 150000,
+    OperatingPin2: 200000,
+    Age: 25,
+    Rating: 2,
+    Experience: 2,
+    Price: 200,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://media.istockphoto.com/id/183298582/photo/profile-of-an-asian-man.jpg?s=612x612&w=0&k=20&c=YzVZSm-x2NbTSFg6xd8TFDez7N4XJam2n9ipWiad-lU=",
+  },
+  {
+    ID: 4,
+    Name: "Sukriti Chad",
+    Address: "10, BaberGunj, Delhi",
+    Phone: "+91 3058469509",
+    Gender: "Female",
+    OperatingPin1: 250000,
+    OperatingPin2: 300000,
+    Age: 25,
+    Rating: 3,
+    Experience: 2,
+    Price: 200,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://www.shutterstock.com/image-photo/profile-picture-smiling-indian-young-260nw-1779265490.jpg",
+  },
+  {
+    ID: 5,
+    Name: "Yash Bhai Manda",
+    Address: "51, Harmada, Bhopal - 546465",
+    Phone: "+91 3058469509",
+    Gender: "Male",
+    OperatingPin1: 500000,
+    OperatingPin2: 550000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://previews.123rf.com/images/mklrnt/mklrnt2001/mklrnt200112148/138260655-profile-view-of-young-handsome-bearded-indian-man-looking-at-camera.jpg",
+  },
+  {
+    ID: 6,
+    Name: "Sai Vijay Patel",
+    Address: "96, Mukti Nagar, Thiruvananthapuram",
+    Phone: "+91 3058469509",
+    Gender: "Female",
+    OperatingPin1: 150000,
+    OperatingPin2: 200000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://files.oyebesmartest.com/uploads/preview/insta-118801856-27s0m3wvj.jpeg",
+  },
+  {
+    ID: 7,
+    Name: "Animesh Basu",
+    Address: "10, Gauransh Apartments, Harmada Mumbai",
+    Phone: "+91 3058469509",
+    Gender: "Male",
+    OperatingPin1: 550000,
+    OperatingPin2: 600000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://imilap.com/profileimages/profile_11%20SDN_01553%20copy.jpg",
+  },
+  {
+    ID: 8,
+    Name: "Animesh Basu",
+    Address: "10, Gauransh Apartments, Harmada Mumbai",
+    Phone: "+91 3058469509",
+    Gender: "Male",
+    OperatingPin1: 550000,
+    OperatingPin2: 600000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://imilap.com/profileimages/profile_11%20SDN_01553%20copy.jpg",
+  },
+  {
+    ID: 9,
+    Name: "Flora Shah",
+    Address: "62, Indira Apartments, BholaGunj Gandhinagar",
+    Phone: "+91 3058469509",
+    Gender: "Female",
+    OperatingPin1: 350000,
+    OperatingPin2: 400000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://t3.ftcdn.net/jpg/03/65/85/20/240_F_365852057_mV52iIJS1xtbyGXvJpYT4YhOQejWdnrF.jpg",
+  },
+  {
+    ID: 10,
+    Name: "Megha Krishna Grewal",
+    Address: "32, Sunita Villas, NamitaGunj Jodhpur",
+    Phone: "+91 3058469509",
+    Gender: "Female",
+    OperatingPin1: 150000,
+    OperatingPin2: 200000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://t3.ftcdn.net/jpg/04/35/60/96/240_F_435609640_poYfXAFPGdFscp4Y3Yyj2e5eHgRipauV.jpg",
+  },
+  {
+    ID: 11,
+    Name: "Chirag Prasad",
+    Address: "71, Nilam Villas, Rajendra Chowk Ajmer",
+    Phone: "+91 3058469509",
+    Gender: "Male",
+    OperatingPin1: 150000,
+    OperatingPin2: 200000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://t4.ftcdn.net/jpg/02/97/24/51/240_F_297245133_gBPfK0h10UM3y7vfoEiBC3ZXt559KZar.jpg",
+  },
+  {
+    ID: 12,
+    Name: "Urmila Chandra",
+    Address: "Street Number 6, Uma Nagar, Begumpet, Hyderabad",
+    Phone: "+91 3058469509",
+    Gender: "Female",
+    OperatingPin1: 500000,
+    OperatingPin2: 550000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://t3.ftcdn.net/jpg/02/81/81/86/240_F_281818663_XXRCNuGktKeZsnknqWkKI0rR4JPWui3H.jpg",
+  },
+  {
+    ID: 13,
+    Name: "Sahil Kohli",
+    Address: "Panta Kaluva Rd, K P Nagar, Benz Circle, Vijayawada",
+    Phone: "+91 3058469509",
+    Gender: "Male",
+    OperatingPin1: 500000,
+    OperatingPin2: 550000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile: "https://as.ftcdn.net/v1/pics/placeholders/spacer.gif",
+  },
+  {
+    ID: 14,
+    Name: "Sneha Hetan Manda",
+    Address: "Opposite AIDC, Zoo Rd, Zoo Tiniali, Guwahati",
+    Phone: "+91 3058469509",
+    Gender: "Female",
+    OperatingPin1: 750000,
+    OperatingPin2: 800000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://t3.ftcdn.net/jpg/02/95/58/22/240_F_295582214_IJrBXV3TJeNavfozclGX88TMVESBMWAV.jpg",
+  },
+  {
+    ID: 15,
+    Name: "Sohail Vala",
+    Address: "Hem Chandra Rd, Latasil, Uzan Bazaar, Guwahati",
+    Phone: "+91 3058469509",
+    Gender: "Male",
+    OperatingPin1: 750000,
+    OperatingPin2: 800000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://t3.ftcdn.net/jpg/02/95/58/22/240_F_295582214_IJrBXV3TJeNavfozclGX88TMVESBMWAV.jpg",
+  },
+  {
+    ID: 16,
+    Name: "Daanish Pratap Saran",
+    Address: "National Highway 333",
+    Phone: "+91 3058469509",
+    Gender: "Male",
+    OperatingPin1: 200000,
+    OperatingPin2: 250000,
+    Age: 28,
+    Rating: 3,
+    Experience: 5,
+    Price: 500,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://t4.ftcdn.net/jpg/02/75/71/91/240_F_275719182_pDCahZWZC7z1T2un7xDyw7I09g6CYxPE.jpg",
+  },
+  {
+    ID: 17,
+    Name: "Random",
+    Address: "National Highway 333",
+    Phone: "+91 3058469509",
+    Gender: "Male",
+    OperatingPin1: 100000,
+    OperatingPin2: 850000,
+    Age: 28,
+    Rating: 4,
+    Experience: 20,
+    Price: 1000,
+    Summery:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis labore consequatur deserunt veniam nam dolore distinctio atque aliquid, quia reiciendis qui dolor et nemo, voluptates pariatur velit illo alias ducimus!",
+    Profile:
+      "https://t4.ftcdn.net/jpg/02/75/71/91/240_F_275719182_pDCahZWZC7z1T2un7xDyw7I09g6CYxPE.jpg",
+  },
+];
+
+export default sitterInformation;
+
+/*
+  100000 - 150000
+  150000 - 200000   3
+  200000 - 250000
+  250000 - 300000   1
+  300000 - 350000
+  350000 - 400000   1
+  400000 - 450000   1
+  450000 - 500000   1
+  500000 - 550000   1
+  550000 - 600000   2
+  600000 - 650000
+  650000 - 700000
+  700000 - 750000
+  750000 - 800000 2
+  800000 - 850000
+  */

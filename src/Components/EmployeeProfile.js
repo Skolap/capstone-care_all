556649a103ea848a76bd7c3af17f4d6a29808466
Shailesh Kolap <skolap22@gmail.com>
2023-01-24 18:04:27 +0530

    Created EmployeeProfile.js

---
+++ src/Components/EmployeeProfile.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import sitterInformation from "./EmployeeInformation";
+import Navbar from "./Navbar";
+
+const EmployeeProfile = () => {
+  //Get id from url
+  const params = useParams();
+
+  //Find object that matches id
+  const employee = sitterInformation.find((sitter) => sitter.ID == params.id);
+
+  return (
+    <div className="flex flex-col gap-1">
+      <Navbar />
+      <div className="w-3/5 mx-auto bg-[#00364E] rounded-xl">
+        <div className="flex p-4 items-end">
+          <div className="">
+            <img
+              className="w-64 h-64 rounded-3xl p-2"
+              src={employee.Profile}
+              alt={employee.Profile}
+            />
+          </div>
+          <div className="p-2 pb-10">
+            <h1 className="text-6xl text-white">{employee.Name}</h1>
+            <div className="flex gap-2 text-gray-400">
+              <div className="text-center m-3">
+                <p className="font-bold">{employee.Experience}</p>
+                <p>Years Experience</p>
+              </div>
+              <div className="text-center m-3">
+                <p className="font-bold">₹{employee.Price}</p>
+                <p>Rate Per Hour</p>
+              </div>
+            </div>
+
+            {/* <p className="">Address: {employee.Address}</p> */}
+          </div>
+        </div>
+      </div>
+      <div className="w-3/5 mx-auto bg-[#00364E] rounded-xl">
+        sfahfljhaskjhfkjah
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeProfile;

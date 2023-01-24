6781a303e1206e3b389e50d97890a19b139ab770
Shailesh Kolap <skolap22@gmail.com>
2023-01-24 18:03:15 +0530

    Complated SearchResultCard.js

--- src/Components/SearchForCareTaker/SearchResultCard.js
+++ src/Components/SearchForCareTaker/SearchResultCard.js
@@ -1,20 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../Navbar";
 
 const SearchResultCard = (props) => {
   console.log(`Props: ${props}`);
+  const link = `/employee/${props.id}`;
   return (
     <div>
-      <div>
-        <img src={props.Profile} alt="" />
-      </div>
-      <div>
-        <p>Name: {props.Name}</p>
-        <p>Address: {props.Address}</p>
-        <p>Gender: {props.Gender}</p>
-        <p>Experience: {props.Experience}</p>
-        <p>Price: {props.Price}</p>
-        <p>Phone: {props.Phone}</p>
-      </div>
+      <Link to={link}>
+        <div className="bg-[#00364E] text-white m-2 flex max-w-6xl mx-auto rounded-xl">
+          <div className="m-5">
+            <img
+              className="w-32 h-32 rounded-full"
+              src={props.Profile}
+              alt=""
+            />
+          </div>
+          <div className="p-2">
+            <p className="text-xl font-bold">{props.Name}</p>
+            <div className="text-gray-300">
+              <p>{props.Address}</p>
+              <p>Gender: {props.Gender}</p>
+              <p>Experience: {props.Experience} Years</p>
+              <p>Price: ₹{props.Price}</p>
+              <p>Phone: {props.Phone}</p>
+            </div>
+          </div>
+          <button>Book Now</button>
+        </div>
+      </Link>
     </div>
   );
 };

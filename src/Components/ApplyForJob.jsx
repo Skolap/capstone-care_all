e9c222b692698a77a1ec8f9790384a4cad1fbacc
Vaibhav kondapuram <vaibhavpawanism@gmail.com>
2023-02-03 19:29:22 +0530

    updated

--- src/Components/ApplyForJob.jsx
+++ src/Components/ApplyForJob.jsx
@@ -1,31 +1,141 @@
 import React, { useState } from "react";
-import Login from "./Login";
+import { db } from "../../FirebaseConfigs/firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
+
 const ApplyForJob = () => {
-  //1 Defining Variable to save email
-  const [email, setEmail] = useState(""); //email is actual value and setEmail is function that sets email value
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [gender, setGender] = useState("male");
+  const [areaCode, setAreaCode] = useState("");
+  const [price, setPrice] = useState(0);
 
-  // 3 This Executes when anything changed in input field
-  function OnChangeHandler(event) {
-    // 4 used setEmail to save value in email we define in const [email, setEmail] = useState("");
-    setEmail(event.target.value);
-  }
+  const [errorMsg, setErrorMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
 
-  // 6 This executes on clicking on button
-  function Submit(event) {
-    event.preventDefault(); //Prevent browser from refreshing
-    // 7 Here we print the user you can do whatever you want with email variable
-    console.log(email);
-  }
+  //   Set Name
+  const nameHandler = (e) => {
+    setName(e.target.value);
+  };
+  //   Set Address
+  const addressHandler = (e) => {
+    setAddress(e.target.value);
+  };
+  //Set gender
+  const genderChangeHandler = (e) => {
+    setGender(e.target.value);
+  };
+  //Set Area
+  const areaChangeHandler = (e) => {
+    setAreaCode(e.target.value);
+  };
+  //   Set Phone
+  const phoneHandler = (e) => {
+    setPhoneNumber(e.target.value);
+  };
+  //   Set Price
+  const priceHandler = (e) => {
+    setPrice(e.target.value);
+  };
+
+  //Execute on submit
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // Add User to 'users' collection in database
+    // Add User to 'users' collection in database
+    addDoc(collection(db, "employees"), {
+      name: name,
+      address: address,
+      phone: phoneNumber,
+      gender: gender,
+      areacode: areaCode,
+      price: price,
+    })
+      .then(() => {
+        setSuccessMsg("New user added successfully");
+        // Clear Form
+        setName("");
+        setAddress("");
+        setPhoneNumber("");
+        setGender("");
+        setAreaCode("");
+        setPrice("");
+      })
+      .catch((error) => {
+        setErrorMsg(error.message);
+      });
+  };
   return (
-    <div>
-      {/*0 create form to display  */}
-      <form>
-        {/* It Shows Input field on webpage */}
-        {/* 2 OnChangeHandler function execute when user type sumeting in it */}
-        <input type="text" placeholder="Email" onChange={OnChangeHandler} />
-
-        {/* 5  Submit function executes when user click on submit*/}
-        <button onClick={Submit}>Submit</button>
+    <div className="flex flex-col items-center">
+      {successMsg && (
+        <>
+          <div>{successMsg}</div>
+        </>
+      )}
+      {errorMsg && (
+        <>
+          <div>{errorMsg}</div>
+        </>
+      )}
+      <form onSubmit={onSubmit} className="mx-auto">
+        {/* Get Name */}
+        <div>
+          <label>Full Name</label>
+          <input
+            onChange={nameHandler}
+            type="text"
+            placeholder="Enter Your Name Here"
+          />
+        </div>
+        {/* Get Gender */}
+        <div>
+          <label>Gender</label>
+          <select value={gender} onChange={genderChangeHandler}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        {/* Get Address */}
+        <div>
+          <label>Address</label>
+          <input
+            onChange={addressHandler}
+            type="text"
+            placeholder="Enter Your Address Here"
+          />
+        </div>
+        {/* Get Phone Number */}
+        <div>
+          <label>Phone Number</label>
+          <input
+            onChange={phoneHandler}
+            type="number"
+            placeholder="Enter Your Phone Here"
+          />
+        </div>
+        {/* Get Area Code */}
+        <div>
+          <label>Operation Area</label>
+          <select value={areaCode} onChange={areaChangeHandler}>
+            <option value="">-------SELECT YOUR AREA-------</option>
+            <option value="100000 - 20000">100000 - 20000</option>
+            <option value="200000 - 300000">200000 - 300000</option>
+            <option value="300000 - 400000">300000 - 400000</option>
+            <option value="400000 - 500000">400000 - 500000</option>
+            <option value="500000 - 600000">500000 - 600000</option>
+          </select>
+        </div>
+        {/* Get Price */}
+        <div>
+          <label>Your Service Price</label>
+          <input
+            onChange={priceHandler}
+            type="number"
+            placeholder="Enter Your Price Here"
+          />
+        </div>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
